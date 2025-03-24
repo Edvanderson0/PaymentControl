dd9db980cb3551d7dd665e8ddf0f0d6d3f28cb72
Edvanderson <edvandersonmarques@hotmail.com>
2025-03-24 16:50:36 -0300

    refactor: Tornando as funcoes assincronas e adicionando um validador para emails existentes no banco de dados

--- src/Backend/UseCases/User/Register/IRegisterUserCase.cs
+++ src/Backend/UseCases/User/Register/IRegisterUserCase.cs
@@ -5,7 +5,7 @@ namespace PaymentControl.UseCases.User.Register
 {
     public interface IRegisterUserCase
     {
-        public ResponseUserDto ExecuteRegister(RequestUserDto user);
+        public Task<ResponseUserDto> ExecuteRegister(RequestUserDto user);
 
     }
 }
--- src/Backend/UseCases/User/Register/RegisterUserCase.cs
+++ src/Backend/UseCases/User/Register/RegisterUserCase.cs
@@ -1,26 +1,45 @@
-﻿using PaymentControl.Dtos.Request.Login;
+﻿using Mapster;
+using PaymentControl.Dtos.Request.Login;
 using PaymentControl.Dtos.Response.Login;
 using PaymentControl.Exceptions;
 using PaymentControl.Exceptions.ExceptionBase;
-using PaymentControl.Services.LoginServices;
+using PaymentControl.Infraestructure.Data.Repositories.User;
+using PaymentControl.Services.Validators.User;
 
 namespace PaymentControl.UseCases.User.Register
 {
     public class RegisterUserCase : IRegisterUserCase
     {
-        public ResponseUserDto ExecuteRegister(RequestUserDto user)
+        private IUserRepository _userRepository;
+        public RegisterUserCase(IUserRepository userRepository)
         {
-            Validate(user);
+            _userRepository = userRepository;
+        }
+
+        public async Task<ResponseUserDto> ExecuteRegister(RequestUserDto user)
+        {
+            await Validate(user);
+            var userToRepository = new Entities.User();
+            user.Adapt(userToRepository);
+            await _userRepository.Add(userToRepository);
             var response = new ResponseUserDto();
             response.Name = user.Name;
             return response;
         }
 
-        private static void Validate (RequestUserDto user)
+        private async Task Validate (RequestUserDto user)
         {
             var validator = new RegisterUserValidator();
-            var result = validator.Validate(user);
-            
+            var result = await validator.ValidateAsync(user);
+            var emailExists = await _userRepository.VerifyEmail(user.Email);
+            if (emailExists)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure
+                    (
+                        string.Empty,
+                        ResourceMessageException.EMAIL_EXISTS
+                    ));
+            }
             if (result.IsValid == false)
             {
                 var errorMessage = result.Errors.Select(error => error.ErrorMessage).ToList();
