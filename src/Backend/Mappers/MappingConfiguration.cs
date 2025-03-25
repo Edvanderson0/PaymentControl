41cf9edf87af014b631305550ee09a04798ed35c
Edvanderson <edvandersonmarques@hotmail.com>
2025-03-24 21:57:17 -0300

    feat: adicionando a configuracao do mapster

---
+++ src/Backend/Mappers/MappingConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Mapster;
+
+namespace PaymentControl.Mappers
+{
+    public static class MappingConfiguration
+    {
+        public static IServiceCollection RegisterMaps(this IServiceCollection services)
+        {
+            services.AddMapster();
+
+            return services;
+        }
+    }
+}
