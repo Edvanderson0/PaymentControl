1da2d83c6e1fffbc3cfef3f32bf9b8e3090d9c26
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:18:03 -0300

    feat: encriptando senha com bcrypt e gerando token para controller

--- src/Backend/UseCases/User/Register/RegisterUserCase.cs
+++ src/Backend/UseCases/User/Register/RegisterUserCase.cs
@@ -2,32 +2,44 @@
 using PaymentControl.Dtos.Request.Login;
 using PaymentControl.Dtos.Response.Login;
 using PaymentControl.Exceptions;
-using PaymentControl.Exceptions.ExceptionBase;
+using PaymentControl.Exceptions.ExceptionBase.ExceptionFilter;
 using PaymentControl.Infraestructure.Data.Repositories.User;
+using PaymentControl.Services.EncryptManangment;
+using PaymentControl.Services.Token;
 using PaymentControl.Services.Validators.User;
 
 namespace PaymentControl.UseCases.User.Register
 {
     public class RegisterUserCase : IRegisterUserCase
     {
-        private IUserRepository _userRepository;
-        public RegisterUserCase(IUserRepository userRepository)
+        private readonly IUserRepository _userRepository;
+        private readonly IEncryptManangment _encryptManangment;
+        private readonly ITokenServices _tokenServices;
+        private readonly IGenericRepository<Entities.User> _genericRepository;
+        public RegisterUserCase(IUserRepository userRepository, IEncryptManangment encryptManangment, ITokenServices tokenService, IGenericRepository<Entities.User> genericRepository)
         {
             _userRepository = userRepository;
+            _encryptManangment = encryptManangment;
+            _tokenServices = tokenService;
+            _genericRepository = genericRepository;
         }
 
-        public async Task<ResponseUserDto> ExecuteRegister(RequestUserDto user)
+        public async Task<ResponseUserDto> ExecuteRegister(RequestRegisterUserDto user)
         {
             await Validate(user);
+            string passwordEncrypted = _encryptManangment.Encrypt(user.Password);
+            user.Password = passwordEncrypted;
             var userToRepository = new Entities.User();
             user.Adapt(userToRepository);
-            await _userRepository.Add(userToRepository);
+            var generatedToken = _tokenServices.TokenGenerator(user.Email);
+            await _genericRepository.Add(userToRepository);
             var response = new ResponseUserDto();
             response.Name = user.Name;
+            response.Token = generatedToken;
             return response;
         }
 
-        private async Task Validate (RequestUserDto user)
+        private async Task Validate (RequestRegisterUserDto user)
         {
             var validator = new RegisterUserValidator();
             var result = await validator.ValidateAsync(user);
