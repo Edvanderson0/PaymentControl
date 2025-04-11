5da5714a145c073030368e4e8a90d4a1e6de1b46
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:35:13 -0300

    feat: extensao para configuracao do JWT token

---
+++ src/Backend/Extension/Token/JwtConfigExtension.cs
@@ -0,0 +1,39 @@
+﻿using System.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace PaymentControl.Extension.Token
+{
+    public static class JwtConfigExtension
+    {
+        public static IServiceCollection AddAutentication(this IServiceCollection service, IConfiguration configuration)
+        {
+            var key = Encoding.ASCII.GetBytes(configuration["JwtSetting:Secret"]);
+
+            service.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = configuration["JwtSetting:Issuer"],
+                    ValidAudience = configuration["JwtSetting:Audience"],
+                    ClockSkew = TimeSpan.FromMinutes(1)
+                    
+                };
+            });
+            service.AddAuthorization();
+            return service;
+        }
+    }
+}
