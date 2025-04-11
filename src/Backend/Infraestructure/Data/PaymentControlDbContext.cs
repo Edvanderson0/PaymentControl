4ab053bb7c309f911ef318c74077f6f3069d43ad
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:02:30 -0300

    feat: adicionando tabelas ao banco de dados

--- src/Backend/Infraestructure/Data/PaymentControlDbContext.cs
+++ src/Backend/Infraestructure/Data/PaymentControlDbContext.cs
@@ -7,5 +7,8 @@ namespace PaymentControl.Infraestructure.Data
     {
         public PaymentControlDbContext(DbContextOptions options) : base(options) { }
         public DbSet<User> Users { get; set; }
+        public DbSet<ServiceEntrance> Services { get; set; }
+        public DbSet<ServiceExit> ServiceOutputs { get; set; }
+        public DbSet<PaymentReceived> PaymentsReceived { get; set; }
     }
 }
