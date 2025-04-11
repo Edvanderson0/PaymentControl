7a1e1d29900708a740d55e8b1e7f4111b22bd8eb
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 21:44:54 -0300

    feat: adicionando update, changePassword e deleteUser

--- src/Backend/Controllers/UserController.cs
+++ src/Backend/Controllers/UserController.cs
@@ -1,26 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PaymentControl.Dtos.Request.Login;
+using PaymentControl.Dtos.Request.User;
+using PaymentControl.Dtos.Response.ErrorResponse;
 using PaymentControl.Dtos.Response.Login;
+using PaymentControl.UseCases.User.ChangePassword;
+using PaymentControl.UseCases.User.Delete;
 using PaymentControl.UseCases.User.Register;
+using PaymentControl.UseCases.User.Update;
 
 namespace PaymentControl.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/User")]
     [ApiController]
     public class UserController : ControllerBase
     {
         private readonly IRegisterUserCase _registerUserCase;
-        public UserController(IRegisterUserCase registerUser)
+        private readonly IUpdateUserUseCase _updateUserCase;
+        private readonly IChangeUserPasswordUseCase _changeUserPasswordUseCase;
+        private readonly IDeleteUserUseCase _deleteUserUseCase;
+        
+        public UserController(IRegisterUserCase registerUser, IUpdateUserUseCase updateUser, IChangeUserPasswordUseCase changePassword, IDeleteUserUseCase deleteUser)
         {
             _registerUserCase = registerUser;
+            _updateUserCase = updateUser;
+            _changeUserPasswordUseCase = changePassword;
+            _deleteUserUseCase = deleteUser;
         }
 
-        [HttpPost]
+        [HttpPost("register-user")]
         [ProducesResponseType(typeof(ResponseUserDto), StatusCodes.Status201Created)]
-        public IActionResult RegisterUser (RequestUserDto UserLogin)
+        public async Task<IActionResult> RegisterUser (RequestRegisterUserDto request)
         {
-            var response = _registerUserCase.ExecuteRegister(UserLogin);
+            var response = await _registerUserCase.ExecuteRegister(request);
             return Created(string.Empty, response);
+        }
+
+        [HttpPut("update-user")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorDto), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> UpdateUser (RequestUpdateUserDto request)
+        {
+            await _updateUserCase.ExecuteUpdate(request);
+            return NoContent();
+        }
+
+        [HttpPut("change-password")]
+        [ProducesResponseType (StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorDto), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> ChangePassword(RequestChangeUserPassword request)
+        {
+            await _changeUserPasswordUseCase.Execute(request);
+            return NoContent();
+        }
+
+        [HttpDelete("delete-password")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ResponseErrorDto), StatusCodes.Status404NotFound)]
+        public async Task<IActionResult>DeleteUser(RequestDeleteUserDto request)
+        {
+            await _deleteUserUseCase.Delete(request);
+            return NoContent();
         }
     }
 }
---
+++ src/Backend/Services/Validators/User/RegisterUserValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using PaymentControl.Dtos.Request.Login;
+using PaymentControl.Exceptions;
+namespace PaymentControl.Services.Validators.User
+{
+    public class RegisterUserValidator : AbstractValidator<RequestUserDto>
+    {
+        public RegisterUserValidator()
+        {
+            RuleFor(user => user.Name).NotEmpty().WithMessage(ResourceMessageException.NAME_EMPTY);
+            RuleFor(user => user.Email).NotEmpty().WithMessage(ResourceMessageException.EMAIL_EMPTY);
+            RuleFor(user => user.Email).EmailAddress().WithMessage(ResourceMessageException.EMAIL_INVALID);
+            RuleFor(user => user.Password).NotEmpty().WithMessage(ResourceMessageException.PASSWORD_EMPTY);
+        }
+    }
+}
