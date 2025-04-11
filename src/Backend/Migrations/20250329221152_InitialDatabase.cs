f9acf165d01590d9246fd3779092c8ac3ba54c39
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:40:12 -0300

    feat: primeiro update para o banco informando as entidades de services. serviceOutputs e payamentsReceived

---
+++ src/Backend/Migrations/20250329221152_InitialDatabase.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace PaymentControl.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    NumberTelephone = table.Column<string>(type: "longtext", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Email = table.Column<string>(type: "longtext", nullable: false),
+                    Password = table.Column<string>(type: "longtext", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    IdClientsId = table.Column<int>(type: "int", nullable: false),
+                    ServiceType = table.Column<string>(type: "longtext", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    EntranceDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    UnitValue = table.Column<double>(type: "double", nullable: false),
+                    TotalValue = table.Column<double>(type: "double", nullable: false),
+                    Description = table.Column<string>(type: "longtext", nullable: true),
+                    Status = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Services_Clients_IdClientsId",
+                        column: x => x.IdClientsId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "PaymentsReceived",
+                columns: table => new
+                {
+                    ServiceIdId = table.Column<int>(type: "int", nullable: false),
+                    DataPayment = table.Column<DateOnly>(type: "date", nullable: false),
+                    Value = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_PaymentsReceived_Services_ServiceIdId",
+                        column: x => x.ServiceIdId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ServiceOutputs",
+                columns: table => new
+                {
+                    ServiceIdId = table.Column<int>(type: "int", nullable: false),
+                    ExitDate = table.Column<DateOnly>(type: "date", nullable: false),
+                    Quantities = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_ServiceOutputs_Services_ServiceIdId",
+                        column: x => x.ServiceIdId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentsReceived_ServiceIdId",
+                table: "PaymentsReceived",
+                column: "ServiceIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceOutputs_ServiceIdId",
+                table: "ServiceOutputs",
+                column: "ServiceIdId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_IdClientsId",
+                table: "Services",
+                column: "IdClientsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentsReceived");
+
+            migrationBuilder.DropTable(
+                name: "ServiceOutputs");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
