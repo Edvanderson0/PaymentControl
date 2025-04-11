cb36eab010099ff6cb6e18cf2d31f896ef15e680
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 23:33:37 -0300

    feat: implementacao da funcionalidade de executar a exclusao de um user

---
+++ src/Backend/UseCases/User/Delete/DeleteUserUseCase.cs
@@ -0,0 +1,46 @@
+﻿using PaymentControl.Dtos.Request.User;
+using PaymentControl.Exceptions;
+using PaymentControl.Exceptions.ExceptionBase.ExceptionFilter;
+using PaymentControl.Infraestructure.Data.Repositories.User;
+using PaymentControl.Services.Validators.User;
+
+namespace PaymentControl.UseCases.User.Delete
+{
+    public class DeleteUserUseCase : IDeleteUserUseCase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IGenericRepository<Entities.User> _genericRepository;
+        public DeleteUserUseCase(IUserRepository user, IGenericRepository<Entities.User> genericRepository)
+        {
+            _userRepository = user;
+            _genericRepository = genericRepository; 
+        }
+
+        public async Task Delete(RequestDeleteUserDto request)
+        {
+            await Validate(request);
+            var user = await _genericRepository.GetById(request.Id);
+            _genericRepository.Delete(user);
+        }
+
+        private async Task Validate(RequestDeleteUserDto request)
+        {
+            var validator = new DeleteUserValidator();
+            var result = await validator.ValidateAsync(request);
+            var user = await _genericRepository.GetById(request.Id);
+            if (user == null)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure
+                    (
+                        string.Empty,
+                        ResourceMessageException.INVALID_ID
+                    ));
+            }
+            if(result.IsValid == false)
+            {
+                var errorMessage = result.Errors.Select(errors => errors.ErrorMessage).ToList();
+                throw new ValidatorErrorException(errorMessage);
+            }
+        }
+    }
+}
---
+++ src/Backend/UseCases/User/Delete/IDeleteUserUseCase.cs
@@ -0,0 +1,9 @@
+﻿using PaymentControl.Dtos.Request.User;
+
+namespace PaymentControl.UseCases.User.Delete
+{
+    public interface IDeleteUserUseCase
+    {
+        public Task Delete(RequestDeleteUserDto request);
+    }
+}
