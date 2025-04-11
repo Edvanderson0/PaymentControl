1625b68e80ac5c67c74e0f5385184cfdb5f9d6bb
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:37:40 -0300

    feat: criando repositorio generico, getAll, getByiId, update, add e delete

---
+++ src/Backend/Infraestructure/Data/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace PaymentControl.Infraestructure.Data.Repositories.User
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private readonly PaymentControlDbContext _context;
+
+        public GenericRepository(PaymentControlDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IList<TEntity>> GetAll()
+        {
+            var entitiesList = await _context.Set<TEntity>().ToListAsync();
+            return entitiesList;
+        }
+
+        public async Task<bool> VerifyEmail(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await _context.Set<TEntity>().AnyAsync(predicate);
+        }
+        public async Task<TEntity> GetByEmail(Expression<Func<TEntity, bool>> predicate)
+        {
+            var entityByEmail = await _context.Set<TEntity>().FirstOrDefaultAsync(predicate);
+            return entityByEmail;
+        }
+        public async Task<TEntity> GetById(int id)
+        {
+            var entitie = await _context.Set<TEntity>().FindAsync(id);
+            return entitie;
+        }
+        public async Task Add(TEntity model)
+        {
+            await _context.AddAsync(model);
+            await _context.SaveChangesAsync();
+        }
+        public void Update(TEntity model)
+        {
+            var updatedUser = _context.Update(model);
+            _context.SaveChanges();
+        }
+
+        public void Delete(TEntity model)
+        {
+            var deleteUser = _context.Remove(model);
+            _context.SaveChanges();
+        }
+
+    }    
+}
---
+++ src/Backend/Infraestructure/Data/Repositories/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace PaymentControl.Infraestructure.Data.Repositories.User
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        public Task<IList<TEntity>> GetAll();
+        public Task Add(TEntity model);
+        public Task<TEntity> GetById(int id);
+        public void Update(TEntity model);
+        public void Delete(TEntity model);
+    }
+}
