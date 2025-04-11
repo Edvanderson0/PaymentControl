16772ea67a5ae96b193b43d735806ac27d6d089a
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:08:41 -0300

    feat: implementando injecoes de dependencias

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -1,9 +1,14 @@
-using Mapster;
 using Microsoft.EntityFrameworkCore;
 using PaymentControl.Exceptions.ExceptionBase.ExceptionFilter;
+using PaymentControl.Extension.Token;
 using PaymentControl.Infraestructure.Data;
 using PaymentControl.Infraestructure.Data.Repositories.User;
 using PaymentControl.Mappers;
+using PaymentControl.Services.EncryptManangment;
+using PaymentControl.Services.Token;
+using PaymentControl.UseCases.Login;
+using PaymentControl.UseCases.User.ChangePassword;
+using PaymentControl.UseCases.User.Delete;
 using PaymentControl.UseCases.User.Register;
 using PaymentControl.UseCases.User.Update;
 
@@ -19,16 +24,27 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<PaymentControlDbContext>(option =>
 {
-    option.UseMySql(builder.Configuration.GetConnectionString("mysql"),
-        new MySqlServerVersion(new Version(8,0,4)));
+    option.UseMySQL(connectionString: builder.Configuration["ConnectionString:mysql"]!);
 });
+
 builder.Services.RegisterMaps();
 builder.Services.AddMvc(option => option.Filters.Add(typeof(ExceptionFilter)));
+//EncryptManangment
+builder.Services.AddScoped<IEncryptManangment, EncryptManangment>();
+//Config tokenAutentication
+builder.Services.AddAutentication(builder.Configuration);
+builder.Services.AddScoped<ITokenServices, TokenServices>();
 //UseCases
 builder.Services.AddScoped<IRegisterUserCase, RegisterUserCase>();
 builder.Services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
+builder.Services.AddScoped<IChangeUserPasswordUseCase, ChangeUserPasswordUseCase>();
+builder.Services.AddScoped<IDeleteUserUseCase, DeleteUserUseCase>();
+//Login
+builder.Services.AddScoped<ILoginUseCase, LoginUseCase>();
 //Repository
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -40,8 +56,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
