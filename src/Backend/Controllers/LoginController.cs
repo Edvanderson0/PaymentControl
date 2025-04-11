ec679bbfc6226031c9fffa94bc9f34c3432bdadd
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:21:15 -0300

    feat: criando a controller e implementando o endpoint para fazer o login

---
+++ src/Backend/Controllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PaymentControl.Dtos.Request.Login;
+using PaymentControl.Dtos.Response.ErrorResponse;
+using PaymentControl.Dtos.Response.Login;
+using PaymentControl.UseCases.Login;
+
+namespace PaymentControl.Controllers
+{
+    [Route("Login")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly ILoginUseCase _loginUseCase;
+        public LoginController(ILoginUseCase loginUsecase)
+        {
+            _loginUseCase = loginUsecase;
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseLoginDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ResponseErrorDto), StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login(RequestLoginDto request)
+        {
+            var result = await _loginUseCase.DoLogin(request);
+            return Ok(result);
+        }
+    }
+}
