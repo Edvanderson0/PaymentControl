bc70ddad0a3fc7e9f0642671d0e43a3de9a99395
Edvanderson <edvandersonmarques@hotmail.com>
2025-04-10 22:31:22 -0300

    feat: criando exception personalizada para login

---
+++ src/Backend/Exceptions/ExceptionBase/ExceptionFilter/InvalidLoginException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace PaymentControl.Exceptions.ExceptionBase.ExceptionFilter
+{
+    public class InvalidLoginException : PaymentControlExceptions
+    {
+        private IList<string> _errorMessage;
+        public InvalidLoginException(IList<string> message) : base(string.Empty)
+        {
+            _errorMessage = message;
+        }
+
+        public override IList<string> GetErrorMessages()
+        {
+            return _errorMessage;
+        }
+
+        public override HttpStatusCode GetStatusCode()
+        {
+            return HttpStatusCode.Unauthorized;
+        }
+    }
+}
